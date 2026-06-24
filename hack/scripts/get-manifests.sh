1d387608b8288bf1c036ea755101872286a3399c
Somya Bhatnagar <sbhatnag@redhat.com>
2026-06-24 11:45:01 -0400

    chore: add maas-controller to manifest fetch script
    Add models-as-a-service to get-manifests.sh so CI can fetch
    maas-controller manifests from upstream repo.
    
    Note: Script requires bash 4.0+ for associative arrays. On macOS
    with bash 3.2, manifests are already committed so container-build
    will use them directly.

--- hack/scripts/get-manifests.sh
+++ hack/scripts/get-manifests.sh
@@ -67,7 +67,7 @@ fetch_component() {
 # RHDS commits: https://github.com/red-hat-data-services/<repo_name>/commits/
 declare -A COMPONENTS=(
     [batchgateway]="llm-d-batch-gateway-operator|config|96266f4b7d9d8b4fbe56225a2b37e064717cd3e1|3ffb67bc46bc00e2183df4f568c92d621cdc1743"
-    # [maas]="models-as-a-service|config|<odh_commit>|<rhds_commit>"
+    [maascontroller]="models-as-a-service|deployment/base/maas-controller|c0e01f1185bc4f80cf4a3fecb79f5c6a5e43b96d|"
 )
 
 for component_name in "${!COMPONENTS[@]}"; do
