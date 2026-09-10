ade499d3e49d86cc4682a913fa79d72c2448210e
Wen Zhou <wenzhou@redhat.com>
2026-09-10 10:25:42 +0200

    chore: bump new batch-gateway-operator manfiests
    Signed-off-by: Wen Zhou <wenzhou@redhat.com>

--- hack/scripts/get-manifests.sh
+++ hack/scripts/get-manifests.sh
@@ -67,7 +67,7 @@ fetch_component() {
 # RHDS commits: https://github.com/red-hat-data-services/<repo_name>/commits/
 
 declare -A COMPONENTS=(
-    [batchgateway]="llm-d-batch-gateway-operator|config|b8768fa0654cb4aa2170d52ed81b8235342d3dd3"
+    [batchgateway]="llm-d-batch-gateway-operator|config|c42e63aee41bd84923c6a6ac5576be0b1f83cda3"
     [maascontroller]="models-as-a-service|deployment/base/maas-controller|8f11ce9c7b4bf5ac395d570170ae034fab4d6bc9|8f11ce9c7b4bf5ac395d570170ae034fab4d6bc9"
 )
 
