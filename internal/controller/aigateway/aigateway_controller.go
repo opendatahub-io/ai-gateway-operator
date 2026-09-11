60779944ac7c65d0f1a191574bb8e709d0affe33
Wen Zhou <wenzhou@redhat.com>
2026-09-11 18:01:46 +0200

    fix: allow batch gateway statefulsets
    Signed-off-by: Wen Zhou <wenzhou@redhat.com>

--- internal/controller/aigateway/aigateway_controller.go
+++ internal/controller/aigateway/aigateway_controller.go
@@ -53,7 +53,7 @@ import (
 // +kubebuilder:rbac:groups=dscinitialization.opendatahub.io,resources=dscinitializations,verbs=get;list;watch
 
 // Resources deployed by the batch-gateway operator kustomize manifests
-// +kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=apps,resources=deployments;statefulsets,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups="",resources=services;serviceaccounts;configmaps,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups="",resources=secrets,verbs=create;get;list;patch;watch
 // +kubebuilder:rbac:groups="",resources=events,verbs=create;patch
